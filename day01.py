8f988eef33bd29c48207ef33768ef3957f14398d
Andrew Jones <ajoneshs@gmail.com>
2023-01-16 07:18:27 -0500

    Rename single digit solution file names

---
+++ day01.py
@@ -0,0 +1,11 @@
+input = open("input\day01_input.txt", "r").read()
+
+# a list of the total calories each elf carries
+calorie_inventory = [sum([int(j) for j in i.split('\n')]) for i in input.split("\n\n")] 
+
+# answer to part 1 (calories carried by elf who carries most calories)
+print(max(calorie_inventory))
+
+# answer to part 2 (inventories of top 3 elves)
+calorie_inventory.sort(reverse = True)
+print(sum(calorie_inventory[0:3]))
