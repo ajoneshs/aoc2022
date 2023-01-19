39cd24b4328c3616dd16649923af435593063c3f
Andrew Jones <ajoneshs@gmail.com>
2023-01-19 02:49:17 -0500

    Add part 1 and 2

--- day11.py
+++ day11.py
@@ -1,11 +1,10 @@
 import re
 
-#input = open("input\day11_input.txt", "r").read().splitlines()
-input = open("input\day11_sample_input.txt", "r").read().split('\n\n')
-
+input = open("input\day11_input.txt", "r").read().split('\n\n')
+#input = open("input\day11_sample_input.txt", "r").read().split('\n\n')
 
 monkeys = []
-inventories = []
+inventories_original = []
 operations = []
 tests = []
 recipient_if_true_list = []
@@ -14,50 +13,76 @@ recipient_if_false_list = []
 def setup(input):
     for block in input:
         split_block = block.splitlines()
-        print(split_block[0])
         monkey = re.findall('\d+', split_block[0])
         inventory = re.findall('\d+', split_block[1])
         operation = re.findall('(\*|\+) (\d+|old)', split_block[2])
-        print(f"operation is {operation}")
-        print(f"operation[0][0] is {operation[0]}")
         test = re.findall('\d+', split_block[3])
         recipient_if_true = re.findall('\d+', split_block[4])
         recipient_if_false = re.findall('\d+', split_block[5])
 
-        # just a placeholder til I figure out how I want to organize things
+        # eventually organize this better
         monkeys.append(int(monkey[0]))
-        inventories.append(inventory)
+        inventories_original.append(inventory)
         operations.append(operation[0])
         tests.append(int(test[0]))
         recipient_if_true_list.append(int(recipient_if_true[0]))
         recipient_if_false_list.append(int(recipient_if_false[0]))
 
-
 setup(input)
+monkey_biz_counter_original = [0] * len(monkeys)
 
-print(monkeys)
-print(inventories)
-print(operations)
-print(tests)
-print(recipient_if_true_list)
-print(recipient_if_false_list)
+# without using a smaller number for the stress factor, this program would 
+# take an absurd time to execute; using the least common multiple makes it
+# much faster and ensures it will still correctly reassign items
+mod = 1
+for i in tests:
+    mod *= i
 
-def one_round():
+# goes through one round, assigns inventory accordingly, and increments 
+# monkey_biz_counter for each item inspection
+def one_round(stress_type, inventories, monkey_biz_counter):
+    trues = []
+    falses = []
+    if stress_type == 'high':
+        stress_factor = mod
+    else:
+        stress_factor = 3
     for monkey in monkeys:
-        for counter, old in enumerate(inventories[monkey]):
+        for old in inventories[monkey]:
+            monkey_biz_counter[monkey] += 1
             if operations[monkey][1] == 'old':
                 new = eval(f"{old}{operations[monkey][0]}{old}")
             else:
                 new = eval(f"{old}{operations[monkey][0]}{operations[monkey][1]}")
-            print(new)
-        # change inventory here
-one_round()
+            worry = new // stress_factor
+            if worry % tests[monkey] == 0:
+                trues.append(worry)
+            else:
+                falses.append(worry)
+        inventories[monkey] = []
+        if trues != []:
+            inventories[recipient_if_true_list[monkey]].extend(trues)
+            trues = []
+        if falses != []:
+            inventories[recipient_if_false_list[monkey]].extend(falses)
+            falses = []
 
-'''
-for round in range(20):
+    return inventories, monkey_biz_counter
 
-# go through 1 round, 20 times
-for round in range(20):
-    one_round()
+# this calculates monkey business (product of two top inspection counts) for
+# a given number of rounds and stress_type
+def monkey_biz(rounds, stress_type):
+    monkey_biz_counter = monkey_biz_counter_original.copy()
+    inventories = inventories_original.copy()
+    for round in range(rounds):
+        inventories, monkey_biz_counter = one_round(stress_type, inventories, monkey_biz_counter)
+    first = max(monkey_biz_counter)
+    monkey_biz_counter.remove(first)
+    second = max(monkey_biz_counter)
+    return first*second
 
-'''
+# answer to part 1 (level of monkey business after 20 rounds)
+print(monkey_biz(20, 'low'))
+
+# answer to part 2 (level of monkey business after 10000 rounds)
+print(monkey_biz(10000, 'high'))
