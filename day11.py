2064fa80dd15ccb37568763e8a73426c05bd7894
Andrew Jones <ajoneshs@gmail.com>
2023-01-18 23:15:55 -0500

    Change variable names

--- day11.py
+++ day11.py
@@ -4,15 +4,10 @@ import re
 input = open("input\day11_sample_input.txt", "r").read().split('\n\n')
 
 
-'''
-for block in input:
-    for line in block.splitlines():
-        a
-'''
-namelist = []
-inventorylist = []
-operationlist = []
-testlist = []
+monkeys = []
+inventories = []
+operations = []
+tests = []
 recipient_if_true_list = []
 recipient_if_false_list = []
 
@@ -20,7 +15,7 @@ def setup(input):
     for block in input:
         split_block = block.splitlines()
         print(split_block[0])
-        name = re.findall('\d+', split_block[0])
+        monkey = re.findall('\d+', split_block[0])
         inventory = re.findall('\d+', split_block[1])
         operation = re.findall('(\*|\+) (\d+|old)', split_block[2])
         test = re.findall('\d+', split_block[3])
@@ -28,19 +23,19 @@ def setup(input):
         recipient_if_false = re.findall('\d+', split_block[5])
 
         # just a placeholder til I figure out how I want to organize things
-        namelist.append(name)
-        inventorylist.append(inventory)
-        operationlist.append(operation)
-        testlist.append(test)
+        monkeys.append(monkey)
+        inventories.append(inventory)
+        operations.append(operation)
+        tests.append(test)
         recipient_if_true_list.append(recipient_if_true)
         recipient_if_false_list.append(recipient_if_false)
 
 
 setup(input)
 
-print(namelist)
-print(inventorylist)
-print(operationlist)
-print(testlist)
+print(monkeys)
+print(inventories)
+print(operations)
+print(tests)
 print(recipient_if_true_list)
 print(recipient_if_false_list)
