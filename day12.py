b9af76f562a18548563c0ef57e87c9b02716eb49
unknown <ajoneshs@gmail.com>
2023-02-01 22:03:43 -0500

    Add initial draft of day12

---
+++ day12.py
@@ -0,0 +1,24 @@
+"""AOC Day 12 Solutions"""
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#inpt = open("input\\day12_input.txt", "r", encoding="utf-8").read().splitlines()
+inpt = open("input\\day12_sample_input.txt", "r", encoding="utf-8").read().splitlines()
+
+# Problem:
+# Want to get from S (elevation a) to E (elevation z)
+# Can go up 1 in height or down an unlimited amount
+# Can only move to adjacent positions, not diagonal ones
+
+
+# need to use directed graphs because you may be able to jump down but not back up
+G = nx.digraph()
+# generate graph from inpt here
+for row in inpt:
+    for pos in row:
+# note location of S and change it to its elevation (0/a)
+# note location of E and change it to its elevation (25/z)
+    
+
+nx.draw(G)
+plt.show()
